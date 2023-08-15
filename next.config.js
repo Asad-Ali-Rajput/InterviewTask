878ca055da41ec07643e672337cfe0c4526348b2
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-08-16 04:09:43 +0500

    Updated and Bug Fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['reqres.in'], // Add reqres.in to the domains array
+  },
 }
 
 module.exports = nextConfig
